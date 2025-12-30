9d399488fd2accee8b89365dccf1123a4c5d8ea2
Michelle Rodriguez <promichfsd@gmail.com>
2025-12-30 13:12:14 -0300

    tenemos funcional los tres flujos basicos. phase 1 , facturacion automatica.  y ticket

--- src/jobs/emitInvoicesJob.js
+++
@@ -1,6 +0,0 @@
-import { emitInvoicesForReadyTickets } from "../invoices.js";
-
-console.log("[job] emitInvoicesJob start");
-emitInvoicesForReadyTickets()
-  .then((r) => console.log("[job] emitInvoicesJob done", r))
-  .catch(console.error);
