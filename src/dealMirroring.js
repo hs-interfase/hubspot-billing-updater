15ab7ff95d67fb05740a7a46d5a41c714540c57a
Michelle Rodriguez <promichfsd@gmail.com>
2026-01-18 19:01:23 -0300

    correccion

--- src/dealMirroring.js
+++ src/dealMirroring.js
@@ -14,6 +14,7 @@ import { upsertUyLineItem } from './services/mirrorLineItemsUyUpsert.js';
 
 
 
+
 // Propiedad del line item (checkbox) que marca la línea como operada en UY.
 const LINEA_PARA_UY_PROP = 'uy';
 
