294157ab835dc5ab240c1524afa3f6b6198a443e
Michelle Rodriguez <promichfsd@gmail.com>
2026-02-19 19:14:51 -0300

    cambio el formato de server.js

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 // server.js
 import express from 'express'
-import { createRequire } from 'module'
 import escucharCambios from './api/escuchar-cambios.js'
 import actualizarWebhook from './api/actualizar-webhook.js'
-
-const require = createRequire(import.meta.url)
-const updateBilling = require('./api/update-billing.js')
 
 const app = express()
 app.use(express.json())
 
 app.post('/api/escuchar-cambios', escucharCambios)
 app.post('/api/actualizar-webhook', actualizarWebhook)
-app.post('/api/update-billing', updateBilling)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
